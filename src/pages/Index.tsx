6981c585cb415973763af17dfbc88affbbb379c4
ishhumn <theshumanhere@gmail.com>
2024-12-30 05:19:06 +0545

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,22 +1,19 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import Features from "../components/Features";
-import Stats from "../components/Stats";
+import AboutUs from "../components/AboutUs";
+import Services from "../components/Services";
 import Testimonials from "../components/Testimonials";
-import Blog from "../components/Blog";
 import Footer from "../components/Footer";
 import ChatInterface from "../components/ChatInterface";
-
 const Index = () => {
   return (
     <div className="min-h-screen">
       <Header />
       <main>
         <Hero />
-        <Features />
-        <Stats />
+        <AboutUs />
+        <Services />
         <Testimonials />
-        <Blog />
       </main>
       <Footer />
       <ChatInterface />
