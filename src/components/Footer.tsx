1af6ef8edd1850d49cc5f05c0740f2a66a4fdfd0
ishhumn <theshumanhere@gmail.com>
2024-12-30 05:01:23 +0545

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
         </div>
         <div className="max-w-4xl mx-auto mt-12 pt-8 border-t border-neutral-800">
           <p className="text-neutral-400 text-center">
-            © 2024 Monet. All rights reserved.
+
           </p>
         </div>
       </div>
